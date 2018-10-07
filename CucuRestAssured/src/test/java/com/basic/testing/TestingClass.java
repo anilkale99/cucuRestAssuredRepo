fd5a72a3f1063826ec2abc1e0e640ab0f5d6769c
admin <admin@DESKTOP-EKBA8NT>
2018-10-07 12:41:54 +0530

    third commit

--- CucuRestAssured/src/test/java/com/basic/testing/TestingClass.java
+++ CucuRestAssured/src/test/java/com/basic/testing/TestingClass.java
@@ -3,6 +3,7 @@ package com.basic.testing;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
